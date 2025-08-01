639f7bc13a871f584643fd8601d83ea44513ea8e
Illia Ivanov <Illuha.Iva@gmail.com>
2025-08-01 16:55:11 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+from os.path import isfile
+
+
+def copy_file(command: str) -> None:
+    if len(command.split(" ")) == 3:
+
+        copy, file_to_copy, copied_file = command.split(" ")
+        if (file_to_copy != copied_file and copy == "cp"
+                and isfile(file_to_copy)):
+
+            with (open(file_to_copy, "r")
+                  as file_in, open(copied_file, "w") as file_out):
+                for line in file_in:
+                    file_out.write(line)
