dc50538a611ce10147acc8351fb269db234cdd55
Illia Ivanov <Illuha.Iva@gmail.com>
2025-08-01 17:05:00 +0200

    fixed_solution

--- app/main.py
+++ app/main.py
@@ -2,13 +2,14 @@ from os.path import isfile
 
 
 def copy_file(command: str) -> None:
-    if len(command.split(" ")) == 3:
+    if len(command.split(" ")) != 3:
+        print("invalid command")
 
-        copy, file_to_copy, copied_file = command.split(" ")
-        if (file_to_copy != copied_file and copy == "cp"
-                and isfile(file_to_copy)):
+    copy, file_to_copy, copied_file = command.split(" ")
+    if (file_to_copy != copied_file and copy == "cp"
+            and isfile(file_to_copy)):
 
-            with (open(file_to_copy, "r")
-                  as file_in, open(copied_file, "w") as file_out):
-                for line in file_in:
-                    file_out.write(line)
+        with (open(file_to_copy, "r")
+              as file_in, open(copied_file, "w") as file_out):
+            for line in file_in:
+                file_out.write(line)
